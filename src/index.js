414ae10a93271aff514439a7655ebb18668f9fc0
j-Riv <jriv@suavecito.com>
2019-03-28 12:12:23 -0700

    Cleanup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
-import createHashHistory from 'history/createHashHistory';
 
-const hashHistory = createHashHistory({basename: process.env.PUBLIC_URL});
-
-ReactDOM.render(<Router history={hashHistory}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
