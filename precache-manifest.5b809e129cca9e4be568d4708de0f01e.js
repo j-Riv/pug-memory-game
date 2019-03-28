9b280ae467fcdd7e100cbda0b8f18ba35b5535da
j-Riv <jriv@suavecito.com>
2019-03-28 10:37:27 -0700

    Updates

---
+++ precache-manifest.5b809e129cca9e4be568d4708de0f01e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc6a5ef8a5a4dc08b65c",
+    "url": "/clicky-memory-game/static/js/runtime~main.808650b0.js"
+  },
+  {
+    "revision": "fc8b19203d6d7a1ca944",
+    "url": "/clicky-memory-game/static/js/main.bb879211.chunk.js"
+  },
+  {
+    "revision": "3e30fef8ea5331d65989",
+    "url": "/clicky-memory-game/static/js/2.cc3c387b.chunk.js"
+  },
+  {
+    "revision": "fc8b19203d6d7a1ca944",
+    "url": "/clicky-memory-game/static/css/main.790b0fb9.chunk.css"
+  },
+  {
+    "revision": "87a68ef86c63571569f752212088217b",
+    "url": "/clicky-memory-game/index.html"
+  }
+];
